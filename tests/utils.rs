ea0d726f101bba05e8b2d0fc16072b46036afc4a
fomotoshi <fomotoshi@ya.ru>
2022-02-18 15:46:15 +0800

    add new feature for testing. every test create fresh database.

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,9 +1,49 @@
 use std::net::TcpListener;
 
-pub fn spawn_app() -> String {
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
+
+use battlemon_rest::config;
+use battlemon_rest::config::DatabaseSettings;
+
+pub async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = battlemon_rest::run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut config = config::get_config().expect("Failed to read configuration");
+    config.database.database_name = Uuid::new_v4().to_string();
+    let db_pool = configure_db(&config.database).await;
+
+    let server =
+        battlemon_rest::startup::run(listener, db_pool.clone()).expect("Failed to bind address");
+
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    TestApp { address, db_pool }
+}
+
+pub async fn configure_db(config: &DatabaseSettings) -> PgPool {
+    let mut conn = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    conn.execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
+
+    let conn_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    sqlx::migrate!("./migrations")
+        .run(&conn_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    conn_pool
+}
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
 }
