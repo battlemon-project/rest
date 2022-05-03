2ae43928c2e2948bfd7c72707175f201d94c9595
ChzenChzen <fedorovdanila@me.com>
2022-05-04 00:16:28 +0800

    implement base functionality of nft_tokens route

--- src/routes/nft_tokens.rs
+++ src/routes/nft_tokens.rs
@@ -1,6 +1,27 @@
 use crate::routes::Pagination;
 use actix_web::{web, HttpResponse};
+use chrono::{DateTime, Utc};
+use nft_models::ModelKind;
+use serde::{Deserialize, Serialize};
+use sqlx::types::Json;
 use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NftToken {
+    pub id: Uuid,
+    pub owner_id: String,
+    pub token_id: String,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub media: String,
+    pub media_hash: Option<String>,
+    pub copies: Option<String>,
+    pub issued_at: Option<String>,
+    pub expires_at: Option<String>,
+    pub model: Json<ModelKind>,
+    pub db_created_at: DateTime<Utc>,
+}
 
 pub async fn nft_tokens(
     pagination: web::Query<Pagination>,
@@ -10,14 +31,14 @@ pub async fn nft_tokens(
     let offset = pagination.offset.unwrap_or_default();
 
     let rows = sqlx::query_as!(
-        TokenExt,
+        NftToken,
         r#"
-        SELECT * FROM nft_tokens ORDER BY id LIMIT $1 OFFSET $2;
+        SELECT id, owner_id, token_id, media, model as "model: Json<ModelKind>", db_created_at, copies, description, expires_at, issued_at, title, media_hash
+        FROM nft_tokens ORDER BY id LIMIT $1 OFFSET $2;
         "#,
         limit,
         offset
-    )
-    .fetch_all(pool.get_ref())
+    ).fetch_all(pool.get_ref())
     .await;
 
     match rows {
