08af12081681c8f74e08c1e48f8650e804fcf222
ChzenChzen <fedorovdanila@me.com>
2022-05-07 02:55:16 +0800

    implement `Dummy` for `Sale`

--- tests/dummies.rs
+++ tests/dummies.rs
@@ -1,15 +1,35 @@
-use chrono::{DateTime, Utc};
-use fake::{Dummy, Fake};
+use fake::faker::chrono::raw::DateTime;
+use fake::faker::lorem::en::Word;
+use fake::faker::number::raw::NumberWithFormat;
+use fake::locales::EN;
+use fake::uuid::UUIDv4;
+use fake::{Dummy, Fake, Faker};
+use rand::Rng;
 use serde::Deserialize;
-use uuid::Uuid;
 
-#[derive(Debug, Dummy, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, Clone)]
 pub struct Sale {
-    pub id: Uuid,
+    pub id: uuid::Uuid,
     pub prev_owner: String,
     pub curr_owner: String,
     pub token_id: String,
-    #[dummy(faker = "1..1000000000")]
-    pub price: i64,
-    pub date: DateTime<Utc>,
+    pub price: rust_decimal::Decimal,
+    pub date: chrono::DateTime<chrono::Utc>,
+}
+
+impl Dummy<Faker> for Sale {
+    fn dummy_with_rng<R: Rng + ?Sized>(_: &Faker, rng: &mut R) -> Self {
+        let scale = rng.gen_range(0..=24);
+        let lo = rng.gen();
+        let mid = rng.gen();
+        let price = rust_decimal::Decimal::from_parts(lo, mid, 0, false, scale);
+        Self {
+            id: UUIDv4.fake::<uuid::Uuid>(),
+            prev_owner: format!("{}.near", Word().fake::<String>()),
+            curr_owner: format!("{}.near", Word().fake::<String>()),
+            token_id: NumberWithFormat(EN, "^########").fake::<String>(),
+            price,
+            date: DateTime(EN).fake(),
+        }
+    }
 }
