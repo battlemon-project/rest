f0f9b99dd898604852b4b9d56729280559396575
ChzenChzen <fedorovdanila@me.com>
2022-05-07 23:14:31 +0800

    instrument `health_check`

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,3 +1,4 @@
+#[tracing::instrument(name = "Health check", fields(request_id = %uuid::Uuid::new_v4()))]
 pub async fn health_check() -> actix_web::HttpResponse {
     actix_web::HttpResponse::Ok().finish()
 }
