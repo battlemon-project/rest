54df24d3e85790bddd4f9dcbe8875d2139a6eb87
ChzenChzen <fedorovdanila@me.com>
2022-05-07 23:49:53 +0800

    - instrument tracing for `sale` - instrument tracing for `query_sales`

--- src/routes/sale.rs
+++ src/routes/sale.rs
@@ -1,32 +1,38 @@
+use crate::filter::PaginationFilter;
 use actix_web::{web, HttpResponse};
 use chrono::{DateTime, Utc};
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
-use sqlx::types::Decimal;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-#[derive(Deserialize, Debug)]
-pub struct Filter {
-    pub limit: Option<i64>,
-    pub offset: Option<i64>,
-    pub days: Option<i64>,
-}
-
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Sale {
     pub id: Uuid,
     pub prev_owner: String,
     pub curr_owner: String,
     pub token_id: String,
+    #[serde(with = "rust_decimal::serde::str")]
     pub price: Decimal,
     pub date: DateTime<Utc>,
 }
 
-pub async fn sale(filter: web::Query<Filter>, pool: web::Data<PgPool>) -> HttpResponse {
+#[tracing::instrument(name = "Handle sales request", skip(pool), fields(request_id = %uuid::Uuid::new_v4()))]
+pub async fn sale(filter: web::Query<PaginationFilter>, pool: web::Data<PgPool>) -> HttpResponse {
     let limit = filter.limit.unwrap_or(100);
-    let offset = filter.offset.unwrap_or_default();
+    let offset = filter.offset.unwrap_or(0);
 
-    let rows = sqlx::query_as!(
+    let rows = query_sales(&pool, limit, offset).await;
+
+    match rows {
+        Ok(rows) => HttpResponse::Ok().json(rows),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(name = "Query sales from database", skip(pool))]
+pub async fn query_sales(pool: &PgPool, limit: i64, offset: i64) -> Result<Vec<Sale>, sqlx::Error> {
+    let sales = sqlx::query_as!(
         Sale,
         r#"
         SELECT * FROM sales ORDER BY id LIMIT $1 OFFSET $2;
@@ -34,14 +40,12 @@ pub async fn sale(filter: web::Query<Filter>, pool: web::Data<PgPool>) -> HttpRe
         limit,
         offset
     )
-    .fetch_all(pool.get_ref())
-    .await;
+    .fetch_all(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
 
-    match rows {
-        Ok(rows) => HttpResponse::Ok().json(rows),
-        Err(e) => {
-            println!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    Ok(sales)
 }
