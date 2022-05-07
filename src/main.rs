cc02de99a5d36d18642b38b7121a9f19205071db
ChzenChzen <fedorovdanila@me.com>
2022-05-07 23:21:28 +0800

    - fix log content - fix app's log name

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use battlemon_rest::{config, startup, telemetry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = telemetry::get_subscriber("indexer".into(), "info".into(), std::io::stdout);
+    let subscriber =
+        telemetry::get_subscriber("battlemon_rest".into(), "info".into(), std::io::stdout);
     telemetry::init_subscriber(subscriber);
     tracing::info!("Loading app config");
     let config = config::get_config().expect("Failed to read configuration");
@@ -16,7 +17,7 @@ async fn main() -> std::io::Result<()> {
         .connect_lazy_with(config.database.with_db());
 
     let address = format!("{}:{}", config.application.host, config.application.port);
-    tracing::info!("Binding address for app: {}", address);
+    tracing::info!("Binding address - {address} for app");
     let listener = TcpListener::bind(address)?;
     startup::run(listener, conn_pool)?.await
 }
