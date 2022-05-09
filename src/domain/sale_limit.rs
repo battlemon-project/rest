9edc1c90b0b478e575e015682c6a200702cde8ea
ChzenChzen <fedorovdanila@me.com>
2022-05-09 14:18:24 +0800

    rename signature

--- src/domain/sale_limit.rs
+++ src/domain/sale_limit.rs
@@ -3,9 +3,9 @@ pub struct SaleLimit(i64);
 const DEFAULT_LIMIT: i64 = 100;
 
 impl SaleLimit {
-    pub fn parse(sale_limit: Option<i64>) -> Result<SaleLimit, String> {
-        match sale_limit {
-            Some(n) if n.is_negative() => Err(format!("{sale_limit:?} must be positive")),
+    pub fn parse(limit: Option<i64>) -> Result<SaleLimit, String> {
+        match limit {
+            Some(n) if n.is_negative() => Err(format!("{limit:?} must be positive")),
             Some(0) | None => Ok(Self(DEFAULT_LIMIT)),
             Some(n) => Ok(Self(n)),
         }
