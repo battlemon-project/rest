e9ea3bfee3e103aa23ca69f8a3078791d0a9fe8e
ChzenChzen <fedorovdanila@me.com>
2022-05-09 14:13:14 +0800

    minor fixes

--- src/domain/sale_limit.rs
+++ src/domain/sale_limit.rs
@@ -5,7 +5,7 @@ const DEFAULT_LIMIT: i64 = 100;
 impl SaleLimit {
     pub fn parse(sale_limit: Option<i64>) -> Result<SaleLimit, String> {
         match sale_limit {
-            Some(i64::MIN..=-1) => Err(format!("value inside {sale_limit:?} must be positive")),
+            Some(n) if n.is_negative() => Err(format!("{sale_limit:?} must be positive")),
             Some(0) | None => Ok(Self(DEFAULT_LIMIT)),
             Some(n) => Ok(Self(n)),
         }
