109d181a82840522a33935d3c492def6025a5cfa
ChzenChzen <fedorovdanila@me.com>
2022-05-09 14:17:52 +0800

    rename signature

--- src/domain/sale_offset.rs
+++ src/domain/sale_offset.rs
@@ -1,8 +1,8 @@
 pub struct SaleOffset(i64);
 
 impl SaleOffset {
-    pub fn parse(sale_offset: Option<i64>) -> Result<SaleOffset, String> {
-        match sale_offset {
+    pub fn parse(offset: Option<i64>) -> Result<SaleOffset, String> {
+        match offset {
             Some(n) if n.is_negative() => Err(format!("{sale_offset:?} must be positive")),
             None => Ok(Self(0)),
             Some(n) => Ok(Self(n)),
