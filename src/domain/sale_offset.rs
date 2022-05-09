f1803afcbf9d8333fe4d6663d279f340082a52ea
ChzenChzen <fedorovdanila@me.com>
2022-05-09 14:18:57 +0800

    fix error in error msg

--- src/domain/sale_offset.rs
+++ src/domain/sale_offset.rs
@@ -3,7 +3,7 @@ pub struct SaleOffset(i64);
 impl SaleOffset {
     pub fn parse(offset: Option<i64>) -> Result<SaleOffset, String> {
         match offset {
-            Some(n) if n.is_negative() => Err(format!("{sale_offset:?} must be positive")),
+            Some(n) if n.is_negative() => Err(format!("{offset:?} must be positive")),
             None => Ok(Self(0)),
             Some(n) => Ok(Self(n)),
         }
