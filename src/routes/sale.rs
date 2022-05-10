3dae927218b76a4a3090969d00f705c0f4a99cd2
ChzenChzen <fedorovdanila@me.com>
2022-05-10 16:07:10 +0800

    - add `try_from` for `SaleFilter` - refactor `sale` - refactor `query_sales`

--- src/routes/sale.rs
+++ src/routes/sale.rs
@@ -1,4 +1,5 @@
-use crate::filter::PaginationFilter;
+use super::PaginationQuery;
+use crate::domain::{SaleFilter, SaleLimit, SaleOffset};
 use actix_web::{web, HttpResponse};
 use chrono::{DateTime, Utc};
 use rust_decimal::Decimal;
@@ -17,28 +18,43 @@ pub struct Sale {
     pub date: DateTime<Utc>,
 }
 
+impl TryFrom<PaginationQuery> for SaleFilter {
+    type Error = String;
+
+    fn try_from(value: PaginationQuery) -> Result<Self, Self::Error> {
+        let limit = SaleLimit::parse(value.limit)?;
+        let offset = SaleOffset::parse(value.offset)?;
+        Ok(Self { limit, offset })
+    }
+}
+
 #[tracing::instrument(name = "Handle sales request", skip(pool))]
-pub async fn sale(filter: web::Query<PaginationFilter>, pool: web::Data<PgPool>) -> HttpResponse {
-    let limit = filter.limit.unwrap_or(100);
-    let offset = filter.offset.unwrap_or(0);
+pub async fn sale(
+    web::Query(filter): web::Query<PaginationQuery>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let filter = match filter.try_into() {
+        Ok(f) => f,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
 
-    let query_result = query_sales(limit, offset, &pool).await;
-
+    let query_result = query_sales(filter, &pool).await;
     match query_result {
         Ok(sales) => HttpResponse::Ok().json(sales),
-        Err(e) => HttpResponse::InternalServerError().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[tracing::instrument(name = "Query sales from database", skip(pool))]
-pub async fn query_sales(limit: i64, offset: i64, pool: &PgPool) -> Result<Vec<Sale>, sqlx::Error> {
+pub async fn query_sales(filter: SaleFilter, pool: &PgPool) -> Result<Vec<Sale>, sqlx::Error> {
     let rows = sqlx::query_as!(
         Sale,
         r#"
-        SELECT * FROM sales ORDER BY id LIMIT $1 OFFSET $2;
+        SELECT id, prev_owner, curr_owner, token_id, price, date
+        FROM sales ORDER BY id LIMIT $1 OFFSET $2;
         "#,
-        limit,
-        offset
+        filter.limit(),
+        filter.offset()
     )
     .fetch_all(pool)
     .await
