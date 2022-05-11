36ed04b3bd52798f834740d6c2b0f5d741e4d0a4
ChzenChzen <fedorovdanila@me.com>
2022-05-12 00:37:06 +0800

    fix error formatting

--- src/domain/sale_offset.rs
+++ src/domain/sale_offset.rs
@@ -4,7 +4,9 @@ pub struct SaleOffset(i64);
 impl SaleOffset {
     pub fn parse(offset: Option<i64>) -> Result<SaleOffset, String> {
         match offset {
-            Some(n) if n.is_negative() => Err(format!("{n} must be positive")),
+            Some(n) if n.is_negative() => {
+                Err(format!("The offset value is {n}. It must be positive."))
+            }
             None => Ok(Self(0)),
             Some(n) => Ok(Self(n)),
         }
