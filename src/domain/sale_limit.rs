eeb17444bd93591a47d6aec433b92b61c0ed2699
ChzenChzen <fedorovdanila@me.com>
2022-05-11 22:53:56 +0800

    - fix default values for `SaleLimit`

--- src/domain/sale_limit.rs
+++ src/domain/sale_limit.rs
@@ -7,7 +7,7 @@ impl SaleLimit {
     pub fn parse(limit: Option<i64>) -> Result<SaleLimit, String> {
         match limit {
             Some(n) if n.is_negative() => Err(format!("{limit:?} must be positive")),
-            Some(0) | None => Ok(Self(DEFAULT_LIMIT)),
+            None => Ok(Self(DEFAULT_LIMIT)),
             Some(n) => Ok(Self(n)),
         }
     }
