7d2fbd5119a5087dd081f067f4d4d29337443131
ChzenChzen <fedorovdanila@me.com>
2022-05-12 17:34:19 +0800

    - refactor helpers

---
+++ tests/helpers.rs
@@ -0,0 +1,82 @@
+use std::net::TcpListener;
+
+use actix_web::dev::Server;
+use once_cell::sync::Lazy;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
+
+use battlemon_rest::config::{DatabaseSettings, Settings};
+use battlemon_rest::startup::{get_connection_pool, Application};
+use battlemon_rest::telemetry::{get_subscriber, init_subscriber};
+use battlemon_rest::{config, startup};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+impl TestApp {
+    pub async fn get_sales(&self, query: String) -> reqwest::Response {
+        reqwest::Client::new()
+            .get(&format!("{}/sales?{query}", self.address))
+            .send()
+            .await
+            .expect("Failed to execute request")
+    }
+}
+
+pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
+    let mut config = {
+        let mut cfg = config::get_config().expect("Failed to read configuration");
+        cfg.database.database_name = Uuid::new_v4().to_string();
+        cfg.application.port = 0;
+        cfg
+    };
+
+    let application = Application::build(config.clone())
+        .await
+        .expect("Failed to build application");
+    let address = format!("http://127.0.0.1:{}", application.port());
+    let _ = tokio::spawn(application.run_until_stopped());
+
+    TestApp {
+        address,
+        db_pool: get_connection_pool(&config.database),
+    }
+}
+
+pub async fn configure_db(config: &DatabaseSettings) -> PgPool {
+    let mut conn = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    conn.execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
+
+    let conn_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    sqlx::migrate!("../../migrations/migrations")
+        .run(&conn_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    conn_pool
+}
