58c805f31ba7fb5d6d9cecf800d20a3ee9594e3a
ChzenChzen <fedorovdanila@me.com>
2022-05-12 14:43:05 +0800

    - add test for `SaleDays`

--- src/domain/sale_days.rs
+++ src/domain/sale_days.rs
@@ -4,6 +4,36 @@ pub struct SaleDays;
 impl SaleDays {
     #[allow(unused_variables)]
     pub fn parse(days: Option<i64>) -> Result<(), String> {
-        Err("query 'days' is prohibited for the `sales` route".into())
+        match days {
+            None => Ok(()),
+            Some(_) => Err("query 'days' is prohibited for the `sales` route".into()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn none_value_is_valid_for_being_parsed_successfully_by_sale_days() {
+        let days = None;
+        let actual = SaleDays::parse(days);
+        assert!(
+            actual.is_ok(),
+            "The actual `SaleDays` isn't `Ok`, actual value is {:?}",
+            actual
+        )
+    }
+
+    #[test]
+    fn any_some_values_for_days_is_rejected() {
+        let days = Some(10);
+        let actual = SaleDays::parse(days);
+        assert!(
+            actual.is_err(),
+            "The actual `SaleDays` isn't `Err(..)`, actual value is {:?}",
+            actual
+        )
     }
 }
