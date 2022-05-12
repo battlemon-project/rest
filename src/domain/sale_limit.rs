a91978c04648710c4b54e180bd20a9cad6938485
ChzenChzen <fedorovdanila@me.com>
2022-05-12 10:54:02 +0800

    refactor unittest `SaleLimit`

--- src/domain/sale_limit.rs
+++ src/domain/sale_limit.rs
@@ -25,19 +25,33 @@ impl SaleLimit {
 
 #[cfg(test)]
 mod tests {
-    use claim::{assert_err, assert_ok_eq};
-
     use super::*;
 
     #[test]
     fn negative_limit_is_rejected() {
         let limit = Some(-10);
-        assert_err!(SaleLimit::parse(limit));
+        let actual = SaleLimit::parse(limit);
+        assert!(
+            actual.is_err(),
+            "`SaleLimit` isn't `Err`, actual value is {:?}",
+            actual
+        );
     }
 
     #[test]
     fn none_limit_equals_default() {
         let limit = None;
-        assert_ok_eq!(SaleLimit::parse(limit), SaleLimit::default());
+        let actual = SaleLimit::parse(limit);
+        assert!(
+            actual.is_ok(),
+            "The actual `SaleLimit` isn't `Ok`, actual value is {:?}",
+            actual
+        );
+        assert_eq!(
+            actual,
+            Ok(SaleLimit::default()),
+            "The actual `SaleLimit` doesn't equal default value, actual value is {:?}",
+            actual
+        );
     }
 }
