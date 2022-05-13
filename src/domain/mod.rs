24cf717cb484fd3c8e1181a7bc078d9c193837af
ChzenChzen <fedorovdanila@me.com>
2022-05-13 20:36:21 +0800

    implement trait for common domain purposes

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -11,3 +11,22 @@ mod sale;
 mod sale_days;
 mod sale_limit;
 mod sale_offset;
+
+pub trait New {
+    fn new(value: i64) -> Self;
+}
+
+pub trait ParseToPositiveInt: New + Default
+where
+    Self: Sized,
+{
+    const ERROR: &'static str = "The parsed value must be positive.";
+
+    fn parse(value: Option<i64>) -> Result<Self, &'static str> {
+        match value {
+            Some(v) if v.is_negative() => Err(Self::ERROR),
+            None => Ok(Self::default()),
+            Some(v) => Ok(Self::new(v)),
+        }
+    }
+}
