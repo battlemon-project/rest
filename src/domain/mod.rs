e88f9468603dbec1e3054d88f0759bb54d267c1d
ChzenChzen <fedorovdanila@me.com>
2022-05-13 18:26:47 +0800

    add new modules

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,9 +1,13 @@
-mod sale;
-mod sale_limit;
-mod sale_offset;
-mod sale_days;
-
+pub use paid::*;
+pub use paid_limit::*;
 pub use sale::*;
+pub use sale_days::*;
 pub use sale_limit::*;
 pub use sale_offset::*;
-pub use sale_days::*;
+
+mod paid;
+mod paid_limit;
+mod sale;
+mod sale_days;
+mod sale_limit;
+mod sale_offset;
