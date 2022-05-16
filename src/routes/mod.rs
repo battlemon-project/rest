ecb3c215c1207c5fcd1fa3934fcb0abf9e93d741
ChzenChzen <fedorovdanila@me.com>
2022-05-16 10:54:52 +0800

    minor fixes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,15 +1,14 @@
+use serde::Deserialize;
+
 pub use health_check::*;
 pub use nft_tokens::*;
 pub use paid::*;
 pub use sale::*;
-// todo: add tracing to all routes
+
 mod health_check;
 mod nft_tokens;
 mod paid;
 mod sale;
-
-use serde::Deserialize;
-use std::fmt::Debug;
 
 #[derive(Deserialize, Debug)]
 pub struct PaginationQuery {
