e9c2e3b2e2a8fcdba80e95392f4a1e668d45d751
ChzenChzen <fedorovdanila@me.com>
2022-05-17 15:29:10 +0800

    impl `by_user_id` field for `NftTokenFilter`

---
+++ src/domain/nft_token.rs
@@ -0,0 +1,10 @@
+pub use crate::domain::UserId;
+
+pub struct NftTokenFilter {
+    // pub offset:
+    // pub limit:
+    // pub days:
+    pub by_user_id: UserId,
+    // pub by_token_id: TokenId,
+    // pub by_token_trait: TokenTrait,
+}
