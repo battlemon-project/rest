8b4604d28f7cf869f6da00a783b87cf012f77295
ChzenChzen <fedorovdanila@me.com>
2022-05-19 20:57:28 +0800

    add `nft_tokens_for_valid_query_by_token_id_returns_200` testcasez

--- tests/nft_tokens.rs
+++ tests/nft_tokens.rs
@@ -9,6 +9,57 @@ mod dummies;
 mod helpers;
 
 #[tokio::test]
+async fn nft_tokens_for_valid_query_by_token_id_returns_200() {
+    let app = spawn_app().await;
+    let token_ids = tokens::<5>();
+    let model = ModelKind::Lemon(get_foo_lemon());
+    let owner_id = "alice.near";
+    let ipfs_hash = "somehash";
+
+    for id in token_ids.iter() {
+        sqlx::query!(
+            r#"
+            INSERT INTO nft_tokens (id, owner_id, token_id, media, model, db_created_at)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            "#,
+            Uuid::new_v4(),
+            owner_id,
+            id,
+            ipfs_hash,
+            Json(model.clone()) as _,
+            Utc::now()
+        )
+        .execute(&app.db_pool)
+        .await
+        .expect("Failed to execute query");
+    }
+
+    for expected_token_id in token_ids {
+        let query = format!("token_id={expected_token_id}");
+        let response = app.get_nft_tokens(&query).await;
+        assert!(response.status().is_success());
+
+        let nft_tokens_json = response
+            .json::<Vec<NftToken>>()
+            .await
+            .expect("Couldn't deserialize response into `Vec<NftToken>`");
+        assert_eq!(
+            nft_tokens_json.len(),
+            1,
+            "Expected length `1` for query `{}` and actual doesn't equal.",
+            query
+        );
+
+        let actual_token_id = nft_tokens_json[0].token_id.as_str();
+        assert_eq!(
+            actual_token_id, expected_token_id,
+            "Token id from response `{}` and expected id `{}` doesn't equal.",
+            actual_token_id, expected_token_id
+        );
+    }
+}
+
+#[tokio::test]
 async fn nft_tokens_for_valid_query_by_owner_id_returns_200() {
     let app = spawn_app().await;
     let token_ids = tokens::<5>();
@@ -60,7 +111,6 @@ async fn nft_tokens_for_valid_query_by_owner_id_returns_200() {
             length,
             id
         );
-
         assert!(
             nft_tokens_json.iter().all(|v| v.owner_id == id),
             "Not all tokens belong to user with id `{}`",
