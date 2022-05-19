b1aa9a59abbffb83192fb8a750e486d548bb72f6
ChzenChzen <fedorovdanila@me.com>
2022-05-19 19:21:50 +0800

    add `nft_tokens_query_by_owner_id` test case

--- tests/nft_tokens.rs
+++ tests/nft_tokens.rs
@@ -1,16 +1,17 @@
+use sqlx::types::{chrono::Utc, Json, Uuid};
+
 use battlemon_rest::routes::NftToken;
+use helpers::spawn_app;
 use nft_models::lemon::{Cap, Cloth, Exo, Eyes, Head, Teeth};
 use nft_models::{Lemon, ModelKind};
-use sqlx::types::{chrono::Utc, Json, Uuid};
-use utils::spawn_app;
+
 mod dummies;
-mod utils;
+mod helpers;
 
 #[tokio::test]
-async fn nft_tokens() {
+async fn nft_tokens_query_by_owner_id() {
     let app = spawn_app().await;
-    let owner_id = "owner.testnet";
-    let token_id = "123";
+    let token_id = ["1", "2", "3", "4", "5"];
     let model = ModelKind::Lemon(Lemon {
         cap: Cap::MA01,
         cloth: Cloth::MA01,
@@ -20,38 +21,56 @@ async fn nft_tokens() {
         teeth: Teeth::A01,
     });
 
+    let expected_owners_and_tokens_length = [
+        ("owner1.testnet", 3),
+        ("owner2.testnet", 2),
+        ("others.testnet", 0),
+    ];
     let ipfs_hash = "somehash";
-    sqlx::query!(
-        r#"
-        INSERT INTO nft_tokens (id, owner_id, token_id, media, model, db_created_at)
-        VALUES ($1, $2, $3, $4, $5, $6)
-        "#,
-        Uuid::new_v4(),
-        owner_id,
-        token_id,
-        ipfs_hash,
-        Json(model) as _,
-        Utc::now()
-    )
-    .execute(&app.db_pool)
-    .await
-    .expect("Failed to execute query");
 
-    let token = sqlx::query_as!(
-        NftToken,
-        r#"
-        SELECT id, owner_id, token_id, media, model as "model: Json<ModelKind>", db_created_at, copies, description, expires_at, issued_at, title, media_hash
-        FROM nft_tokens;
-        "#,
-    )
-    .fetch_one(&app.db_pool)
-    .await
-    .unwrap();
+    for (idx, id) in token_id.iter().enumerate() {
+        let owner_id = if idx < 3 {
+            expected_owners_and_tokens_length[0].0
+        } else {
+            expected_owners_and_tokens_length[1].0
+        };
 
-    let client = reqwest::Client::new();
-    let response = client
-        .get(&format!("{}/nft_tokens", app.address))
-        .send()
+        sqlx::query!(
+            r#"
+            INSERT INTO nft_tokens (id, owner_id, token_id, media, model, db_created_at)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            "#,
+            Uuid::new_v4(),
+            owner_id,
+            id,
+            ipfs_hash,
+            Json(model.clone()) as _,
+            Utc::now()
+        )
+        .execute(&app.db_pool)
         .await
-        .expect("Failed to execute request");
+        .expect("Failed to execute query");
+    }
+
+    for (id, length) in expected_owners_and_tokens_length {
+        let query = format!("owner_id={id}");
+        let response = app.get_nft_tokens(&query).await;
+        let nft_tokens_json = response
+            .json::<Vec<NftToken>>()
+            .await
+            .expect("Couldn't deserialize response into `NftToken`");
+        assert_eq!(
+            nft_tokens_json.len(),
+            length,
+            "Expected length `{}` of tokens for owner `{}` and actual doesn't equal.",
+            length,
+            id
+        );
+
+        assert!(
+            nft_tokens_json.iter().all(|v| v.owner_id == id),
+            "Not all tokens belong to user with id `{}`",
+            id
+        );
+    }
 }
