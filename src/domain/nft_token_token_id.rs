2f252f5aa2ecfd9ef97718c99f67fe5ebee2937c
ChzenChzen <fedorovdanila@me.com>
2022-05-22 00:41:48 +0800

    refactor domains for `nft_token` with macros

--- src/domain/nft_token_token_id.rs
+++ src/domain/nft_token_token_id.rs
@@ -1,25 +1,25 @@
-use crate::domain::{impl_as_inner, impl_into_inner, Parse};
+use crate::domain::Parse;
 
 #[derive(Debug)]
-pub struct TokenId(Option<String>);
+pub struct NftTokenTokenId(Option<String>);
 
-impl_into_inner!(TokenId);
-impl_as_inner!(TokenId);
+crate::domain::impl_into_inner!(NftTokenTokenId);
+crate::domain::impl_as_inner!(NftTokenTokenId);
 
-impl Parse<&str> for TokenId {
+impl Parse<&str> for NftTokenTokenId {
     fn parse(token_id: Option<&str>) -> Result<Self, String> {
         match token_id.map(|v| v.trim()) {
-            None => Ok(TokenId(None)),
+            None => Ok(NftTokenTokenId(None)),
             Some(id) if id.is_empty() => Err("Token id is empty".to_string()),
             Some(id) if !id.chars().all(|ch| ch.is_ascii_digit()) => {
                 Err("The token id must contain only digits".to_string())
             }
-            Some(id) => Ok(TokenId(Some(id.to_string()))),
+            Some(id) => Ok(NftTokenTokenId(Some(id.to_string()))),
         }
     }
 }
 
-impl Parse<String> for TokenId {
+impl Parse<String> for NftTokenTokenId {
     fn parse(token_id: Option<String>) -> Result<Self, String> {
         Self::parse(token_id.as_deref())
     }
@@ -46,14 +46,14 @@ mod tests {
 
     #[quickcheck_macros::quickcheck]
     fn valid_token_ids(valid_token_id: ValidTokenIdFixture) -> bool {
-        TokenId::parse(valid_token_id.0).is_ok()
+        NftTokenTokenId::parse(valid_token_id.0).is_ok()
     }
 
     #[test]
     fn user_id_contains_wrong_chars() {
         let invalid_token_ids = ["a", "1a", "a1", "123a", "12.", ".12", "+12", "-12", "1+2"];
         for id in invalid_token_ids {
-            let actual = TokenId::parse(Some(id));
+            let actual = NftTokenTokenId::parse(Some(id));
             assert!(
                 actual.is_err(),
                 "The actual `TokenId` isn't `Err`, actual value {:?}",
@@ -65,7 +65,7 @@ mod tests {
     #[test]
     fn empty_token_id_is_rejected() {
         let id = Some(String::new());
-        let actual = TokenId::parse(id);
+        let actual = NftTokenTokenId::parse(id);
         assert!(
             actual.is_err(),
             "The actual `TokenId` isn't `Err`, actual value {:?}",
@@ -76,7 +76,7 @@ mod tests {
     #[test]
     fn when_token_id_is_none_then_parsed_is_ok_none() {
         let id: Option<String> = None;
-        let actual = TokenId::parse(id);
+        let actual = NftTokenTokenId::parse(id);
         assert!(
             actual.is_ok(),
             "The actual `TokenId` isn't `Ok`, actual value {:?}",
--- src/domain/nft_token_user_id.rs
+++ src/domain/nft_token_user_id.rs
@@ -2,7 +2,7 @@ use std::lazy::SyncLazy;
 
 use regex::Regex;
 
-use crate::domain::{impl_as_inner, impl_into_inner, Parse};
+use crate::domain::Parse;
 
 static RE: SyncLazy<Regex> = SyncLazy::new(|| {
     Regex::new(r#"^(([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+$"#)
@@ -10,25 +10,25 @@ static RE: SyncLazy<Regex> = SyncLazy::new(|| {
 });
 
 #[derive(Debug)]
-pub struct OwnerId(Option<String>);
+pub struct NftTokenOwnerId(Option<String>);
 
-impl_into_inner!(OwnerId);
-impl_as_inner!(OwnerId);
+crate::domain::impl_into_inner!(NftTokenOwnerId);
+crate::domain::impl_as_inner!(NftTokenOwnerId);
 
-impl Parse<String> for OwnerId {
+impl Parse<String> for NftTokenOwnerId {
     fn parse(owner_id: Option<String>) -> Result<Self, String> {
         Self::parse(owner_id.as_deref())
     }
 }
 
-impl Parse<&str> for OwnerId {
+impl Parse<&str> for NftTokenOwnerId {
     fn parse(owner_id: Option<&str>) -> Result<Self, String> {
         match owner_id.map(|v| v.trim()) {
-            None => Ok(OwnerId(None)),
+            None => Ok(NftTokenOwnerId(None)),
             Some(id) if id.len() < 2 => Err(format!("User id `{id:?}` is too short (min 2)")),
             Some(id) if id.len() > 64 => Err(format!("User id `{id:?}` is too long (max 64)")),
             Some(id) if !RE.is_match(id) => Err(format!("User id `{id:?}` contains wrong chars.")),
-            Some(id) => Ok(OwnerId(Some(id.to_string()))),
+            Some(id) => Ok(NftTokenOwnerId(Some(id.to_string()))),
         }
     }
 }
@@ -42,7 +42,7 @@ mod tests {
     #[test]
     fn owner_id_too_short() {
         let id = Some("a");
-        let actual = OwnerId::parse(id);
+        let actual = NftTokenOwnerId::parse(id);
         assert!(
             actual.is_err(),
             "The actual `UserId` isn't `Err`, actual value {:?}",
@@ -53,7 +53,7 @@ mod tests {
     #[test]
     fn owner_id_too_long() {
         let id = "a".repeat(65);
-        let actual = OwnerId::parse(Some(id));
+        let actual = NftTokenOwnerId::parse(Some(id));
         assert!(
             actual.is_err(),
             "The actual `UserId` isn't `Err`, actual value {:?}",
@@ -65,7 +65,7 @@ mod tests {
     fn owner_id_contains_wrong_chars() {
         let invalid_owner_ids = ["fomo@.testnet", "dev-1603749005325-6/432576", "alice;"];
         for id in invalid_owner_ids {
-            let actual = OwnerId::parse(Some(id));
+            let actual = NftTokenOwnerId::parse(Some(id));
             assert!(
                 actual.is_err(),
                 "The actual `UserId` isn't `Err`, actual value {:?}",
@@ -84,7 +84,7 @@ mod tests {
             "dev-1603749005325-6432576",
         ];
         for id in valid_owner_ids {
-            let actual = OwnerId::parse(Some(id));
+            let actual = NftTokenOwnerId::parse(Some(id));
             assert!(
                 actual.is_ok(),
                 "The actual `UserId` isn't `Ok`, actual value {:?}",
@@ -96,7 +96,7 @@ mod tests {
     #[test]
     fn none_is_ok_none() {
         let token_id: Option<String> = None;
-        let actual = OwnerId::parse(token_id);
+        let actual = NftTokenOwnerId::parse(token_id);
         assert!(
             actual.is_ok(),
             "The actual `UserId` isn't `Ok`, actual value {:?}",
