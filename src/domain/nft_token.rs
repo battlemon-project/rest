9cc31d7e45ed899713a8cc2205e57eeb70eb31cd
ChzenChzen <fedorovdanila@me.com>
2022-05-21 22:36:26 +0800

    add getter `limit` to `NftTokenFilter`

--- src/domain/nft_token.rs
+++ src/domain/nft_token.rs
@@ -1,19 +1,24 @@
-pub use crate::domain::OwnerId;
-use crate::domain::{AsInner, IntoInner, TokenId};
+pub use crate::domain::NftTokenOwnerId;
+use crate::domain::{AsInner, IntoInner, NftTokenLimit, NftTokenTokenId};
 
 pub struct NftTokenFilter {
     // pub offset:
-    // pub limit:
+    pub limit: NftTokenLimit,
     // pub days:
-    pub owner_id: OwnerId,
-    pub token_id: TokenId,
+    pub owner_id: NftTokenOwnerId,
+    pub token_id: NftTokenTokenId,
     // pub by_token_trait: TokenTrait,
 }
 
 impl NftTokenFilter {
+    pub fn limit(&self) -> i64 {
+        self.limit.get()
+    }
+
     pub fn owner_id(&self) -> Option<&str> {
         self.owner_id.as_inner()
     }
+
     pub fn token_id(&self) -> Option<&str> {
         self.token_id.as_inner()
     }
