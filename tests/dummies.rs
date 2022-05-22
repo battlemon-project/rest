4a1cf05c043ca01b46aef424d755d30f981b8788
ChzenChzen <fedorovdanila@me.com>
2022-05-22 10:04:38 +0800

    impl `Dummy` for `NftToken`

--- tests/dummies.rs
+++ tests/dummies.rs
@@ -1,9 +1,11 @@
+use fake::faker::address::en::Geohash;
 use fake::faker::chrono::raw::DateTime;
 use fake::faker::lorem::en::Word;
 use fake::faker::number::raw::NumberWithFormat;
 use fake::locales::EN;
 use fake::uuid::UUIDv4;
 use fake::{Dummy, Fake, Faker};
+use nft_models::{Lemon, ModelKind};
 use rand::Rng;
 use serde::Deserialize;
 
@@ -30,6 +32,35 @@ impl Dummy<Faker> for Sale {
             token_id: NumberWithFormat(EN, "^########").fake::<String>(),
             price,
             date: DateTime(EN).fake(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone)]
+pub struct NftToken {
+    pub id: uuid::Uuid,
+    pub owner_id: String,
+    pub token_id: String,
+    pub media: String,
+    pub model: ModelKind,
+    pub db_created_at: chrono::DateTime<chrono::Utc>,
+}
+
+impl Dummy<Faker> for NftToken {
+    fn dummy_with_rng<R: Rng + ?Sized>(_: &Faker, rng: &mut R) -> Self {
+        let probability = fake::vec![u8; 4]
+            .try_into()
+            .expect("Couldn't convert to array with length 4");
+        let model = ModelKind::Lemon(Lemon::from_random(&probability));
+        let token_id = rng.gen::<u64>().to_string();
+        
+        Self {
+            id: UUIDv4.fake::<uuid::Uuid>(),
+            owner_id: format!("{}.near", Word().fake::<String>()),
+            token_id,
+            media: Geohash(24).fake(),
+            model,
+            db_created_at: DateTime(EN).fake(),
         }
     }
 }
