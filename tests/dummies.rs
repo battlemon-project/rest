80221a36132360751b7b15f211053df4b3fc4c4f
ChzenChzen <fedorovdanila@me.com>
2022-05-22 10:50:35 +0800

    imp `Dummy` for `NftToken`

--- tests/dummies.rs
+++ tests/dummies.rs
@@ -46,21 +46,70 @@ pub struct NftToken {
     pub db_created_at: chrono::DateTime<chrono::Utc>,
 }
 
-impl Dummy<Faker> for NftToken {
-    fn dummy_with_rng<R: Rng + ?Sized>(_: &Faker, rng: &mut R) -> Self {
+pub struct AliceNftToken;
+
+impl Dummy<AliceNftToken> for NftToken {
+    fn dummy_with_rng<R: Rng + ?Sized>(_: &AliceNftToken, rng: &mut R) -> Self {
         let probability = fake::vec![u8; 4]
             .try_into()
             .expect("Couldn't convert to array with length 4");
         let model = ModelKind::Lemon(Lemon::from_random(&probability));
         let token_id = rng.gen::<u64>().to_string();
-        
+
         Self {
             id: UUIDv4.fake::<uuid::Uuid>(),
-            owner_id: format!("{}.near", Word().fake::<String>()),
+            owner_id: "alice.near".to_string(),
             token_id,
             media: Geohash(24).fake(),
             model,
             db_created_at: DateTime(EN).fake(),
         }
+    }
+}
+
+pub struct BobNftToken;
+
+impl Dummy<BobNftToken> for NftToken {
+    fn dummy_with_rng<R: Rng + ?Sized>(_: &BobNftToken, _rng: &mut R) -> Self {
+        let alice_token: NftToken = AliceNftToken.fake();
+        Self {
+            owner_id: "bob.near".to_string(),
+            ..alice_token
+        }
+    }
+}
+
+pub struct DannyNftToken;
+
+impl Dummy<DannyNftToken> for NftToken {
+    fn dummy_with_rng<R: Rng + ?Sized>(_: &DannyNftToken, _rng: &mut R) -> Self {
+        let alice_token: NftToken = AliceNftToken.fake();
+        Self {
+            owner_id: "danny.near".to_string(),
+            ..alice_token
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn alice_nft_token_correct() {
+        let token: NftToken = AliceNftToken.fake();
+        assert_eq!(token.owner_id, "alice.near".to_string());
+    }
+
+    #[test]
+    fn bob_nft_token_correct() {
+        let token: NftToken = BobNftToken.fake();
+        assert_eq!(token.owner_id, "bob.near".to_string());
+    }
+
+    #[test]
+    fn danny_nft_token_correct() {
+        let token: NftToken = DannyNftToken.fake();
+        assert_eq!(token.owner_id, "danny.near".to_string());
     }
 }
