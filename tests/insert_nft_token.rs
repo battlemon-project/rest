e19cbb6148af291f1e11a14c76c3c26fe4575c9f
ChzenChzen <fedorovdanila@me.com>
2022-05-26 15:42:51 +0800

    add `insert_nft_token_fails_and_return_500_if_there_is_a_fatal_database_error` test case

--- tests/insert_nft_token.rs
+++ tests/insert_nft_token.rs
@@ -28,7 +28,7 @@ async fn insert_valid_nft_token_success() {
 }
 
 #[tokio::test]
-async fn insert_invalid_nft_token_fails() {
+async fn insert_invalid_nft_token_rejects_and_returns_400_status() {
     let app = spawn_app().await;
     let token = json!({
         "wrong": "token json"
@@ -36,4 +36,22 @@ async fn insert_invalid_nft_token_fails() {
     let response = app.post_nft_token(&token).await;
     assert_eq!(response.status(), 400, "Response status is not `400`");
     assert_json_error(response).await;
+}
+
+#[tokio::test]
+async fn insert_nft_token_fails_and_return_500_if_there_is_a_fatal_database_error() {
+    let app = spawn_app().await;
+    let token: dummies::NftToken = AliceNftToken.fake();
+    sqlx::query!("ALTER TABLE nft_tokens DROP COLUMN owner_id;",)
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+    let response = app.post_nft_token(&token).await;
+    assert_eq!(
+        response.status(),
+        500,
+        "The API didn't return 500 Internal Server Error"
+    );
+
+    assert_json_error(response).await
 }
