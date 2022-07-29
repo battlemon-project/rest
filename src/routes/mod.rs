3618b04f6816b5832ee7f58277466620c3c85625
ChzenChzen <fedorovdanila@me.com>
2022-07-29 18:20:28 +0300

    add report builder for rows

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -10,9 +10,18 @@ mod nft_tokens;
 mod paid;
 mod sale;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Copy, Clone)]
 pub struct PaginationQuery {
     pub days: Option<i64>,
     pub limit: Option<i64>,
     pub offset: Option<i64>,
+}
+
+fn build_report_for_rows<T>(rows: &[T], limit: i64) -> (&[T], bool) {
+    let limit = limit as usize;
+    if rows.is_empty() || rows.len() <= limit {
+        (rows, true)
+    } else {
+        (&rows[..limit], false)
+    }
 }
