99b14591a2fe6218bff252cb1548566338e68e16
ChzenChzen <fedorovdanila@me.com>
2022-08-04 21:53:19 +0300

    - update migrations

--- migrations/20220804132231_create_asks_table.sql
+++ migrations/20220804132231_create_asks_table.sql
@@ -1,10 +1,9 @@
 -- Add migration script here
 create table asks
 (
-    id         varchar primary key,
-    token_id   text    not null,
-    expire_at  timestamptz,
-    account_id text,
-    approve_id bigserial,
-    price      decimal not null
+    id          varchar primary key,
+    token_id    text      not null,
+    account_id  text      not null,
+    approval_id bigserial not null,
+    price       decimal   not null
 )
